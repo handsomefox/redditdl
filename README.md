232d0db678cda25cec47e4c77dffb2431cc6f176
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-09-27 02:06:42 +0300

    cmd/redditdl: use more sensible arg names

--- README.md
+++ README.md
@@ -12,25 +12,25 @@ go build --ldflags "-s -w" -o redditdl
 
 ```text
 Usage of redditdl:
-  -ort string
+  -orientation string
         Specifies the image orientation ("l" for landscape, "p" for portrait, empty for any) (default "")
-  -cnt
+  -count
       Amount of media to download (default 1)
   -dir
       Specifies the directory where to download the media (default "media")
-  -h
+  -height
       minimal height of the media to download
-  -w
+  -width
       minimal width of the media to download
-  -p
+  -progress
       Indicates whether the application will show the download progress
   -sort
       How to sort (controversial, best, hot, new, random, rising, top) (default "top")
   -sub
       Subreddit name (default "wallpaper")
-  -tf
+  -timeframe
       Timeframe from which to get the posts (hour, day, week, month, year, all) (default "all")
-  -v
+  -verbose
       Turns the logging on or off
   -video
       Indicates whether the application should download videos as well
@@ -40,5 +40,5 @@ Usage of redditdl:
 ## Example
 
 ```bash
-redditdl -cnt 5 -dir example
+redditdl -count 5 -dir example -verbose -progress
 ```
--- cmd/redditdl/redditdl.go
+++ cmd/redditdl/redditdl.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"redditdl/pkg/downloader"
 	"redditdl/pkg/logging"
@@ -10,17 +11,17 @@ import (
 func main() {
 	// Command-line arguments.
 	var (
-		verbose     = flag.Bool("v", false, "Turns the logging on or off")
-		progress    = flag.Bool("p", false, "Indicates whether the application will show the download progress")
+		verbose     = flag.Bool("verbose", false, "Turns the logging on or off")
+		progress    = flag.Bool("progress", false, "Indicates whether the application will show the download progress")
 		inclVideo   = flag.Bool("video", false, "Indicates whether the application should download videos as well")
 		subreddit   = flag.String("sub", "wallpaper", "Subreddit name")
 		sorting     = flag.String("sort", "top", "How to sort (controversial, best, hot, new, random, rising, top)")
-		timeframe   = flag.String("tf", "all", "Timeframe from which to get the posts (hour, day, week, month, year, all)")
+		timeframe   = flag.String("timeframe", "all", "Timeframe from which to get the posts (hour, day, week, month, year, all)")
 		directory   = flag.String("dir", "media", "Specifies the directory where to download the media")
-		amount      = flag.Int("cnt", 1, "Amount of media to download")
-		width       = flag.Int("w", 0, "minimal width of the media to download")
-		height      = flag.Int("h", 0, "minimal height of the media to download")
-		orientation = flag.String("ort", "", "image orientation (\"l\" for landscape, \"p\" for portrait, empty for any)")
+		amount      = flag.Int("count", 1, "Amount of media to download")
+		width       = flag.Int("width", 0, "minimal width of the media to download")
+		height      = flag.Int("height", 0, "minimal height of the media to download")
+		orientation = flag.String("orientation", "", "image orientation (\"l\" for landscape, \"p\" for portrait, other for any)")
 	)
 
 	flag.Parse()
@@ -40,6 +41,12 @@ func main() {
 	}
 
 	log := logging.GetLogger(settings.Verbose)
+
+	if len(os.Args) <= 1 {
+		log.Info("No options specified")
+		flag.PrintDefaults()
+		return
+	}
 
 	// Print the configuration
 	log.Debugf("Using parameters: %#v", settings)
