4db9a4e099d95f52e3273b5487cbe24110ec1f59
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-10-07 16:28:47 +0300

    files: introduce files package for file utilities

---
+++ files/files.go
@@ -0,0 +1,128 @@
+// files is a package with utility-like file functions used in redditdl
+package files
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// File is the structure that is saved to disk later.
+type File struct {
+	Name, Extension string
+	Data            []byte
+}
+
+// New return a pointer to a new File.
+func New(name, ext string, data []byte) *File {
+	return &File{
+		Name:      name,
+		Extension: ext,
+		Data:      data,
+	}
+}
+
+// NewFilename generates a valid filename for the media.
+func NewFilename(name, extension string) (string, error) {
+	formatted, err := format(name, extension)
+	if err != nil {
+		return "", fmt.Errorf("error creating filename (%v): %w", name, err)
+	}
+
+	// Resolve duplicates
+	for i := 0; Exists(formatted); i++ {
+		formatted, err = format("("+strconv.Itoa(i)+") "+name, extension)
+		if err != nil {
+			return "", fmt.Errorf("error creating filename (%v): %w", name, err)
+		}
+	}
+
+	return formatted, nil
+}
+
+// Exists returns whether the file exists.
+func Exists(filename string) bool {
+	f, err := os.Stat(filename)
+	if err != nil {
+		return os.IsExist(err)
+	}
+
+	return !f.IsDir()
+}
+
+const MaxFilenameLength = 200
+
+// format ensures that the filename is valid for NTFS and has the right extension.
+func format(filename, extension string) (string, error) {
+	if filename == "" {
+		return "", fmt.Errorf("empty filename provided")
+	}
+
+	if extension == "" {
+		return "", fmt.Errorf("empty extension provided")
+	}
+
+	filename = removeForbiddenChars(filename)
+	extension = removeForbiddenChars(extension)
+
+	totalLength := len(filename) + len(extension) + 1
+	if totalLength > MaxFilenameLength {
+		requiredLength := MaxFilenameLength - len(extension) - 1
+		filename = filename[:requiredLength]
+	}
+
+	return filename + "." + extension, nil
+}
+
+// removeForbiddenChars removes invalid characters for Linux/Windows filenames.
+func removeForbiddenChars(name string) string {
+	var (
+		forbiddenChars = []string{"/", "<", ">", ":", "\"", "\\", "|", "?", "*", "(", ")"}
+		result         = name
+	)
+
+	for _, c := range forbiddenChars {
+		result = strings.ReplaceAll(result, c, "")
+	}
+
+	return result
+}
+
+// NavigateTo moves to the provided directory and creates it if necessary.
+func NavigateTo(dir string, createDir bool) error {
+	if createDir {
+		if err := os.Mkdir(dir, os.ModePerm); err != nil {
+			if !errors.Is(err, os.ErrExist) {
+				return fmt.Errorf("error creating a directory, %w", err)
+			}
+		}
+	}
+
+	if err := os.Chdir(dir); err != nil {
+		return fmt.Errorf("error navigating to directory, %w", err)
+	}
+
+	return nil
+}
+
+// Save saves the file to the provided path/filename.
+func Save(filename string, file *File) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("error creating a file: %w", err)
+	}
+
+	r := bytes.NewReader(file.Data)
+	if _, err := r.WriteTo(f); err != nil {
+		if err := os.Remove(filename); err != nil {
+			return fmt.Errorf("error removing a file: %w", err)
+		}
+
+		return fmt.Errorf("error writing to a file: %w", err)
+	}
+
+	return nil
+}
---
+++ files/files_test.go
@@ -0,0 +1,99 @@
+package files_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/handsomefox/redditdl/files"
+)
+
+func TestCreateFilename(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name, extension, want string
+	}{
+		{"file", "jpg", "file.jpg"},
+		{"//file//", "|||extension", "file.extension"},
+		{"", "png", ""},
+		{"file", "", ""},
+	}
+
+	for _, test := range tests {
+		got, _ := files.NewFilename(test.name, test.extension)
+		if got != test.want {
+			t.Errorf("CreateFilename(%#v) unexpected result, got: %v, want: %v", test, got, test.want)
+		}
+	}
+}
+
+func TestNavigateToDirectory(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		dir         string
+		create      bool
+		shouldError bool
+	}{
+		{os.TempDir(), false, false},
+		{"dir", true, false},
+		{"zzzzzzzzzzzzzzzzz", false, true},
+	}
+
+	for _, test := range tests {
+		err := files.NavigateTo(test.dir, test.create)
+
+		if test.shouldError && err == nil {
+			t.Errorf("NavigateToDirectory(%#v) unexpected result, got: %v, want: %#v", test, err, test.shouldError)
+		}
+
+		if !test.shouldError && err != nil {
+			t.Errorf("NavigateToDirectory(%#v) unexpected result, got: %v, want: %#v", test, err, test.shouldError)
+		}
+	}
+}
+
+func TestFileExists(t *testing.T) {
+	tests := []struct {
+		name         string
+		create, want bool
+	}{
+		{"randomfilename", false, false},
+		{"anotherrandomfilename.exe", false, false},
+		{"randomfilename", true, true},
+		{"anotherrandomfilename.exe", true, true},
+	}
+
+	_ = files.NavigateTo(os.TempDir(), false)
+
+	for index, test := range tests {
+		if !test.create {
+			continue
+		}
+
+		file, err := os.CreateTemp(os.TempDir(), test.name)
+		if err != nil {
+			t.Errorf("Couldn't create file, name %v, dir %v", test.name, os.TempDir())
+
+			continue
+		}
+
+		test.name = filepath.Base(file.Name())
+		tests[index].name = filepath.Base(file.Name())
+
+		file.Close()
+	}
+
+	for _, test := range tests {
+		if got := files.Exists(test.name); got != test.want {
+			t.Errorf("FileExists(%#v) unexpected output, want %v, got %v", test, test.want, got)
+		}
+	}
+
+	for _, test := range tests {
+		if test.create {
+			os.Remove(test.name)
+		}
+	}
+}
