12340d2a5742baf83d2ddcc85d26f101834dba4e
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-10-09 16:42:06 +0300

    fetch: improve tests

--- fetch/fetch_test.go
+++ fetch/fetch_test.go
@@ -1,6 +1,8 @@
 package fetch_test
 
 import (
+	"net/http"
+	"reflect"
 	"testing"
 
 	"github.com/handsomefox/redditdl/fetch"
@@ -8,30 +10,80 @@ import (
 
 func TestNewClient(t *testing.T) {
 	t.Parallel()
-	if client := fetch.NewClient(); client == nil {
-		t.Error("Failed to create client") // this will never happen
+	tests := []struct {
+		name string
+		want *http.Client
+	}{
+		{},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := fetch.NewClient(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewClient() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestIsURL(t *testing.T) {
-	t.Parallel()
-
+	type args struct {
+		str string
+	}
 	tests := []struct {
-		url  string
+		name string
+		args args
 		want bool
 	}{
-		{"google.com", false},
-		{"google", false},
-		{"www.google", false},
-		{"http://google.com", true},
-		{"https://google.com", true},
-		{"http://", false},
+		{
+			name: "http://",
+			args: args{
+				str: "http://",
+			},
+			want: false,
+		},
+		{
+			name: "google.com",
+			args: args{
+				str: "google.com",
+			},
+			want: false,
+		},
+		{
+			name: "google",
+			args: args{
+				str: "google",
+			},
+			want: false,
+		},
+		{
+			name: "www.google",
+			args: args{
+				str: "www.google",
+			},
+			want: false,
+		},
+		{
+			name: "http://google.com",
+			args: args{
+				str: "http://google.com",
+			},
+			want: true,
+		},
+		{
+			name: "https://google.com",
+			args: args{
+				str: "https://google.com",
+			},
+			want: true,
+		},
 	}
-
-	for _, test := range tests {
-		got := fetch.IsURL(test.url)
-		if got != test.want {
-			t.Errorf("TestIsURL(%#v) unexpected result, got: %v, want: %v", test, got, test.want)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fetch.IsURL(tt.args.str); got != tt.want {
+				t.Errorf("IsURL() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
