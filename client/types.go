72636985dd30b8af830158fce8d87d13653e5e47
Eduard Diachenko <handsomefox.hf@gmail.com>
2023-01-19 16:37:01 +0200

    client: combine config.go and types.go

--- client/types.go
+++ client/types.go
@@ -4,6 +4,16 @@ import (
 	"strings"
 )
 
+type Config struct {
+	Subreddit   string
+	Sorting     string
+	Timeframe   string
+	Orientation string
+	Count       int64
+	MinWidth    int
+	MinHeight   int
+}
+
 type ContentType byte
 
 const (
