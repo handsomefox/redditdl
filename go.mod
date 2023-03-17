15fb588b9afc1c7139241c4d707a55c4bd935bf6
Eduard Diachenko <handsomefox.hf@gmail.com>
2023-03-17 18:11:27 +0200

    completely rewrite main.go, remove cobra

--- go.mod
+++ go.mod
@@ -3,14 +3,14 @@ module github.com/handsomefox/redditdl
 go 1.20
 
 require (
+	github.com/alexflint/go-arg v1.4.3
 	github.com/rs/zerolog v1.29.0
-	github.com/spf13/cobra v1.6.1
 )
 
 require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/alexflint/go-scalar v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "github.com/handsomefox/redditdl/cmd"
+import (
+	"os"
+
+	"github.com/alexflint/go-arg"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type AppArguments struct {
+	SubredditContentType string   `arg:"-t, --type" help:"content type, values: image,video,both" default:"image"`
+	SubredditSort        string   `arg:"-s, --sort" help:"sorting, values: controversial/best/hot/new/random/rising/top" default:"top"`
+	SubredditTimeframe   string   `arg:"-f, --timeframe" help:"timeframe, values: hour/day/week/month/year/all" default:"all"`
+	SubredditList        []string `arg:"-r, --subreddits" help:"a comma-separated list of subreddits to download from"`
+	SubredditShowNSFW    bool     `arg:"-n, --nsfw" help:"enable if you want to show NSFW content"`
+
+	MediaCount       int64  `arg:"-c, --count" help:"amount of media to download"`
+	MediaOrientation string `arg:"-o, --orientation" help:"values: landspace/portrait/both" default:"both"`
+	MediaWidth       int    `arg:"-w, --width" help:"minimal content width"`
+	MediaHeight      int    `arg:"-h, --height" help:"minimal content height"`
+
+	SaveDirectory string `arg:"-d, --dir" help:"output path"`
+
+	VerboseLogging  bool `arg:"-v, --verbose" help:"enable debug logging"`
+	ProgressLogging bool `arg:"-p, --progress" help:"enable current progress logging"`
+}
 
 func main() {
-	cmd.Execute()
+	var args AppArguments
+	p := arg.MustParse(&args)
+
+	if args.SaveDirectory == "" {
+		p.Fail("you must provide a valid output path using -d or --dir")
+	}
+	if len(args.SubredditList) == 0 {
+		p.Fail("you must provide a list of comma-separated subreddits using -r or --subreddits")
+	}
+	if args.MediaCount == 0 {
+		log.Info().Msg("no media requested to download, ending")
+		os.Exit(0)
+	}
+	if args.VerboseLogging {
+		log.Logger = log.Level(zerolog.InfoLevel)
+	} else {
+		log.Logger = log.Level(zerolog.DebugLevel)
+	}
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	log.Debug().Any("app_arguments", args).Send()
+
+	if err := run(); err != nil {
+		log.Fatal().Err(err).Msg("error running the app")
+	}
+}
+
+func run() error {
+	return nil
 }
