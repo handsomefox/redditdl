34d75526f625247105dbc61eeddf9089cea552bf
Eduard Diachenko <handsomefox.hf@gmail.com>
2023-03-18 23:44:14 +0200

    redditdl: remove old code

--- client/errors.go
+++
@@ -1,9 +0,0 @@
-package client
-
-import "errors"
-
-var (
-	ErrCreateRequest     = errors.New("error creating a request")
-	ErrInvalidStatusCode = errors.New("invalid status code")
-	ErrDoRequest         = errors.New("error performing request to reddit api")
-)
